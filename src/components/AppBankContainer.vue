77e3cfa7b5cfa2f0ff16f2371b6c201fb5960c33
SheilAPK <karaulov.z@mail.ru>
2025-01-09 15:35:43 +0300

    Попытался написать асинхронный  запрос

--- src/components/AppBankContainer.vue
+++ src/components/AppBankContainer.vue
@@ -14,7 +14,7 @@
             <app-category img="package.svg" noActive>5% Активный отдых</app-category>
             <app-category img="package.svg" noActive>5% Красота</app-category>
         </div>
-        <div id_b="2" class="bank__add-category add-category popup-link visible" href="#popup-categories">
+        <div id_b="2" class="bank__add-category add-category popup-link visible" href="#popup-categories" @click="async">
             <div class="add-category__icon">
                 <img src="../assets/img/icons/plus.svg" alt="">
             </div>
@@ -28,11 +28,36 @@ import AppCategory from "./AppCategory.vue";
 
 export default {
     components: { AppCategory },
+    methods: {
+        async() {
+            // // Создаём новый объект XMLHttpRequest
+            // var xhr = new XMLHttpRequest();
+
+            // // Настраиваем запрос: метод GET, адрес URL
+            // xhr.open('GET', '../assets/async.php', true); 
+
+            // // Определяем функцию, которая будет вызвана при получении ответа
+            // xhr.onload = function () {
+            // if (xhr.status >= 200 && xhr.status < 300) {
+            //     // Заказ успешно получен, выводим список материалов
+            //     console.log('Список материалов:', xhr.responseText);
+            // } else {
+            //     // Произошла ошибка при получении заказа
+            //     console.error('Ошибка загрузки:', xhr.statusText);
+            // }
+            // };
+
+            // // Отправляем запрос
+            // xhr.send();
+        },
+    }
 }
 
 </script>
 
 <style lang="scss">
+    // Нужно ещё убрать выделение плюсика в добавлении категории 
+
     .bank {
         &__header {
             display: flex;
