228dfddf8b021c2287425a7f5aec479a9fb696dc
SheilAPK <karaulov.z@mail.ru>
2025-01-17 15:59:52 +0300

    Подключил библиотеку vue-meta

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import TheHeader from './components/TheHeader'
+import { createMetaManager } from 'vue-meta';
+
 
 const app = createApp(App)
-    app.component('the-header', TheHeader)
-    app.use(router)
-    app.mount('#app')
+const metaManager = createMetaManager();
+
+
+app.component('the-header', TheHeader)
+app.use(metaManager);
+app.use(router)
+app.mount('#app')
