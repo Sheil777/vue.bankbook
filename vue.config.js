729bf8df8f2d72b2a818bfae452c122da1c96c02
SheilAPK <karaulov.z@mail.ru>
2025-04-03 08:51:50 +0300

    Теперь это pwa приложение

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pwa: {
+    name: 'BankBook',
+    themeColor: '#FFFFFF',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black-translucent',
+    manifestOptions: {
+      background_color: '#FFFFFF',
+      icons: [
+        {
+          src: "favicon.png",
+          sizes: "90x90",
+          type: "image/png"
+        },
+      ]
+    },
+  }
 })
