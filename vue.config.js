473afa6e31442aa6e0dd476563cd102e38cba7b6
Sheil777APK <karaulov.z@mail.ru>
2025-06-27 11:09:40 +0300

    Исправлена ошибка с фавиконкой

--- vue.config.js
+++ vue.config.js
@@ -7,21 +7,36 @@ module.exports = defineConfig({
     msTileColor: '#000000',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black-translucent',
+    
+    // Добавляем пути к иконкам для разных случаев
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/msapplication-icon-144x144.png'
+    },
+
     manifestOptions: {
       version: '1.0.0',
       background_color: '#FFFFFF',
       icons: [
         {
-          src: "favicon.png",
-          sizes: "90x90",
-          type: "image/png"
+          src: 'img/icons/android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
         },
         {
-          src: "favicon.png",
-          sizes: "192x192",
-          type: "image/png"
+          src: 'img/icons/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
         },
+        {
+          src: 'img/icons/apple-touch-icon.png',
+          sizes: '180x180',
+          type: 'image/png'
+        }
       ]
-    },
+    },  
   }
 })
